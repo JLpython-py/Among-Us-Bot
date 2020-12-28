83afce680639f57bc8777ba260ba018d23489c3b
Jacob Lee <JLpython@outlook.com>
2020-12-28 14:53:47 -0800

    Refactor commands, class ScrollingEmbeds; add 'maps' command

--- bots.py
+++ bots.py
@@ -22,7 +22,6 @@ class MapBot(commands.Bot):
             self, command_prefix=command_prefix, self_bot=False)
         self.name = name
         self.directory = directory
-        self.categories = ['actions', 'locations', 'tasks', 'vents']
         self.data = {}
         self.scrolling_embeds = {}
         self.read_files()
@@ -31,15 +30,18 @@ class MapBot(commands.Bot):
     def read_files(self):
         ''' Read CSV data for each map
 '''
-        for cat in self.categories:
-            path = os.path.join('data', self.directory, cat, f"{cat}.csv")
-            with open(path) as file:
+        dirpath = os.path.join('data', self.directory)
+        directories = [d for d in os.listdir(dirpath)\
+                       if os.path.isdir(os.path.join(dirpath, d))]
+        for direct in directories:
+            csvfile = os.path.join(dirpath, direct, f"{direct}.csv")
+            with open(csvfile) as file:
                 data = list(csv.reader(file))
                 headings = data.pop(0)
-            self.data[cat] = {}
+            self.data[direct] = {}
             for row in data:
                 info = dict(zip(headings, row))
-                self.data[cat].setdefault(
+                self.data[direct].setdefault(
                     info['Name'], info)
 
     async def on_ready(self):
@@ -52,132 +54,100 @@ class MapBot(commands.Bot):
             return
         name = payload.emoji.name
         if name in [
-            u'\u23ee', u'\u23ea', u'\u25c0', u'\u25b6', u'\u23e9', u'\u23ed']:
-            scrolling_embed = self.scrolling_embeds[payload.member.id]
-            await scrolling_embed.scroll(payload)
+            u'\u23ee', u'\u23ea', u'\u25c0', u'\u25b6', u'\u23e9',
+            u'\u23ed']:
+            embed = self.scrolling_embeds[payload.member.id]
+            await embed.scroll(payload)
         elif name == u'\u274c':
-            channel = self.get_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
+            embed = self.scrolling_embeds[payload.member.id]
+            await embed.message.delete()
             del self.scrolling_embeds[payload.member.id]
-            await message.delete()
 
-    def map_information_embed(self, data, option, category):
-        embed = discord.Embed(
-            title=f"{category.title()}: {option}",
-            color=0x0000ff)
-        for item in data:
-            embed.add_field(name=item, value=data[item])
-        image_name = f"{data['Name']}.png"
-        image_path = os.path.join(
-            'data', self.directory, category, image_name)
-        image = discord.File(image_path, image_name)
-        embed.set_image(url=f"attachment://{image_name}")
-        return image, embed
+    async def map_information(self, ctx, *, category, option):
+        items = self.data.get(category)
+        option = option.title()
+        data = items.get(option)
+        if data is None:
+            embed = ScrollingEmbed(
+                self.directory, ctx.message, self.data,
+                category=category)
+            embed.manage_embed()
+            await embed.send_with_reactions()
+            if ctx.message.author.id in self.scrolling_embeds:
+                embed = self.scrolling_embeds[ctx.message.author.id]
+                await embed.message.delete()
+                del self.scrolling_embeds[ctx.message.author.id]
+            self.scrolling_embeds[ctx.message.author.id] = embed
+            await ctx.message.delete()
+        else:
+            embed = discord.Embed(
+                title=f"{category.title()}: {option}",
+                color=0x0000ff)
+            for item in data:
+                embed.add_field(name=item, value=data[item])
+            image_name = f"{data['Name']}.png"
+            image_path = os.path.join(
+                'data', self.directory, category, image_name)
+            image = discord.File(image_path, image_name)
+            embed.set_image(url=f"attachment://{image_name}")
+            await ctx.send(file=image, embed=embed)
+            await ctx.message.delete()
 
     def execute_commands(self):
         ''' MapBot-class commands which can be used by members
 '''
         @self.command(name="actions", pass_context=True, aliases=["a"])
         async def actions(ctx, option=''):
-            actions = self.data.get('actions')
-            option = option.title()
-            data = actions.get(option)
-            if data is None:
-                embed = ScrollingEmbed(
-                    self.directory, ctx.message, self.data,
-                    category='actions')
-                await embed.create()
-                self.scrolling_embeds.setdefault(
-                    ctx.message.author.id, embed)
-                await ctx.message.delete()
-            else:
-                image, embed = self.map_information_embed(
-                    data, option,
-                    category='actions')
-                await ctx.send(file=image, embed=embed)
+            await self.map_information(
+                ctx, category='actions', option=option)
 
         @self.command(name="locations", pass_context=True, aliases=["l"])
         async def locations(ctx, option=''):
-            actions = self.data.get('locations')
-            option = option.title()
-            data = actions.get(option)
-            if data is None:
-                embed = ScrollingEmbed(
-                    self.directory, ctx.message, self.data,
-                    category='locations')
-                await embed.create()
-                self.scrolling_embeds.setdefault(
-                    ctx.message.author.id, embed)
-                await ctx.message.delete()
-            else:
-                image, embed = self.map_information_embed(
-                    data, option,
-                    category='locations')
-                await ctx.send(file=image, embed=embed)
+            await self.map_information(
+                ctx, category='locations', option=option)
 
         @self.command(name="tasks", pass_context=True, aliases=["t"])
         async def tasks(ctx, option=''):
-            actions = self.data.get('tasks')
-            option = option.title()
-            data = actions.get(option)
-            if data is None:
-                embed = ScrollingEmbed(
-                    self.directory, ctx.message, self.data,
-                    category='tasks')
-                await embed.create()
-                self.scrolling_embeds.setdefault(
-                    ctx.message.author.id, embed)
-                await ctx.message.delete()
-            else:
-                image, embed = self.map_information_embed(
-                    data, option,
-                    category='tasks')
-                await ctx.send(file=image, embed=embed)
+            await self.map_information(
+                ctx, category='tasks', option=option)
 
         @self.command(name="vents", pass_context=True, aliases=["v"])
         async def vents(ctx, option=''):
-            actions = self.data.get('vents')
-            option = option.title()
-            data = actions.get(option)
-            if data is None:
-                embed = ScrollingEmbed(
-                    self.directory, ctx.message, self.data,
-                    category='vents')
-                await embed.create()
-                self.scrolling_embeds.setdefault(
-                    ctx.message.author.id, embed)
-                await ctx.message.delete()
-            else:
-                image, embed = self.map_information_embed(
-                    data, option,
-                    category='vents')
-                await ctx.send(file=image, embed=embed)
+            await self.map_information(
+                ctx, category='vents', option=option)
+
+        @self.command(name="maps", pass_context=True, aliases=["m"])
+        async def maps(ctx, option=''):
+            await self.map_information(
+                ctx, category='maps', option=option)
 
 class ScrollingEmbed:
     def __init__(self, directory, message, data, category):
-        self.message = message
-        self.channel = self.message.channel
-        self.memberid = self.message.author.id
+        self.channel = message.channel
+        self.memberid = message.author.id
         self.directory = directory
         self.category = category
         self.items = data.get(self.category)
-        self.option = list(self.items)[0]
-        self.data = self.items.get(self.option)
 
-    async def create(self):
+    def manage_embed(self, index=0):
+        self.option = list(self.items)[index]
+        self.data = self.items.get(self.option)
         self.embed = discord.Embed(
             title=f"{self.category.title()}: {self.option}",
             color=0x0000ff)
+        self.embed.set_footer(
+            text=f"Page {index+1}/{len(self.items)}")
         for item in self.data:
             self.embed.add_field(name=item, value=self.data[item])
         image_name = f"{self.data['Name']}.png"
         image_path = os.path.join(
             'data', self.directory, self.category, image_name)
-        image = discord.File(image_path, image_name)
+        self.image = discord.File(image_path, image_name)
         self.embed.set_image(url=f"attachment://{image_name}")
-        self.embed.set_footer(
-            text=f"Page 1/{len(self.items)}")
-        self.message = await self.channel.send(file=image, embed=self.embed)
+
+    async def send_with_reactions(self):
+        self.message = await self.channel.send(
+            file=self.image, embed=self.embed)
         reactions = [
             u'\u23ee', u'\u23ea', u'\u25c0', u'\u25b6', u'\u23e9', u'\u23ed',
             u'\u274c']
@@ -185,7 +155,6 @@ class ScrollingEmbed:
             await self.message.add_reaction(rxn)
 
     async def scroll(self, payload):
-        #Verify that the member requested the embed
         if payload.member.id != self.memberid:
             await self.message.remove_reaction(
                 payload.emoji, payload.member)
@@ -195,20 +164,7 @@ class ScrollingEmbed:
             u'\u23ee': 0, u'\u23ea': index-5, u'\u25c0': index-1,
             u'\u25b6': index+1, u'\u23e9': index+5, u'\u23ed': -1}
         index = scroll.get(payload.emoji.name)%len(self.items)
-        self.option = list(self.items)[index]
-        self.data = self.items.get(self.option)
-        self.embed = discord.Embed(
-            title=f"{self.category.title()}: {self.option}",
-            color=0x0000ff)
-        for item in self.data:
-            self.embed.add_field(name=item, value=self.data[item])
-        image_name = f"{self.data['Name']}.png"
-        image_path = os.path.join(
-            'data', self.directory, self.category, image_name)
-        image = discord.File(image_path, image_name)
-        self.embed.set_image(url=f"attachment://{image_name}")
-        self.embed.set_footer(
-            text=f"Page {index+1}/{len(self.items)}")
+        self.manage_embed(index=index)
         await self.message.edit(embed=self.embed)
         await self.message.remove_reaction(payload.emoji, payload.member)
         
