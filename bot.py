e4bbc3ba6af27d61a1950ca25560ddbe930b3e3b
Jacob Lee <JLpython@outlook.com>
2021-01-14 15:11:34 -0800

    Add randomize command which invokes other commands in RandomAmongUs cog

--- bot.py
+++ bot.py
@@ -66,6 +66,31 @@ class RandomAmongUs(commands.Cog):
             self.settings = json.load(file)
         self.maps = ["MIRA HQ", "Polus", "The Skeld"]
 
+    @commands.command(name="randomize", pass_context=True, aliases=["r"])
+    async def randomize(self, ctx):
+        ''' Invoke all commands in RandomAmongUs
+'''
+        for cmd in self.get_commands():
+            if cmd.name == "randomize":
+                continue
+            await ctx.invoke(cmd)
+
+    @commands.command(name="randmap", pass_context=True, aliases=["rm"])
+    async def randmap(self, ctx):
+        ''' Select random map
+'''
+        num = random.randint(1, 3)
+        mapname = random.choice(self.maps)
+        embed = discord.Embed(title="Randomize Map", color=0xff0000)
+        embed.add_field(name="Map", value=mapname)
+        embed.add_field(name="Impostors", value=num)
+        thumb_name = f"{''.join(mapname.split()).lower()}.png"
+        thumb_path = os.path.join(
+            'data', thumb_name)
+        thumbnail = discord.File(thumb_path, thumb_name)
+        embed.set_thumbnail(url=f"attachment://{thumb_name}")
+        await ctx.channel.send(file=thumbnail, embed=embed)
+
     @commands.command(name="randsettings", pass_context=True, aliases=["rs"])
     async def randsettings(self, ctx, setting=''):
         ''' Select random option for specified setting
@@ -86,20 +111,6 @@ class RandomAmongUs(commands.Cog):
         for field in fields:
             embed.add_field(name=field, value=fields[field])
         await ctx.send(embed=embed)
-
-    @commands.command(name="randmap", pass_context=True, aliases=["rm"])
-    async def randmap(self, ctx):
-        ''' Select random map
-'''
-        mapname = random.choice(self.maps)
-        embed = discord.Embed(title="Randomize Map", color=0xff0000)
-        embed.add_field(name="Map", value=mapname)
-        thumb_name = f"{''.join(mapname.split()).lower()}.png"
-        thumb_path = os.path.join(
-            'data', thumb_name)
-        thumbnail = discord.File(thumb_path, thumb_name)
-        embed.set_thumbnail(url=f"attachment://{thumb_name}")
-        await ctx.channel.send(file=thumbnail, embed=embed)
 
 class MapInfo(commands.Cog):
     ''' Allow member to explore available information in Among Us
